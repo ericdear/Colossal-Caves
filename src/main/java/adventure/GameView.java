52b891b6d25cf4a13ff71d1602e8b2aba4261428
Eric Dearing <eric@dearing.ca>
2020-06-13 14:21:22 -0400

    now using jfilechooser

--- src/main/java/adventure/GameView.java
+++ src/main/java/adventure/GameView.java
@@ -3,6 +3,7 @@ package adventure;
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -15,10 +16,11 @@ import javax.swing.border.EmptyBorder;
 import java.awt.Container;
 import java.awt.Font;
 import java.awt.Insets;
+import java.io.File;
+import java.io.IOException;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.GridLayout;
-
 
 public class GameView extends JFrame {
     private static final long serialVersionUID = 7305436428373534270L;
@@ -46,10 +48,11 @@ public class GameView extends JFrame {
     private JTextArea inventory = new JTextArea();
 
     private JScrollPane scrollPane;
-    //JScrollBar scrollBar;
-    
+    // JScrollBar scrollBar;
+
     /**
      * the main of the GameView, sets up GameView gui
+     * 
      * @param args
      */
     public static void main(String[] args) {
@@ -60,15 +63,16 @@ public class GameView extends JFrame {
 
     /**
      * sets up the gui
+     * 
      * @param newGame - the game object
-     * @param args - the command line arguments
+     * @param args    - the command line arguments
      */
     public GameView(Game newGame, String[] args) {
         super();
-        //super.setBackground(Color.BLACK);
+        // super.setBackground(Color.BLACK);
         game = newGame;
         adventure = game.gameIntro(args);
-        
+
         setUpSize();
         setMainContainer();
         setAdventure(args);
@@ -76,11 +80,12 @@ public class GameView extends JFrame {
 
     /**
      * sets up the adventure
+     * 
      * @param args - the command line arguments
      */
     private void setAdventure(String[] args) {
         filename = game.setFileName(args, adventure);
-        
+
         checkNullAdventure();
         outputArea.append(filename + " has been loaded\n\n");
 
@@ -91,12 +96,12 @@ public class GameView extends JFrame {
     }
 
     /**
-     * checks for the adventure being null
-     * null adventure is caused by the wrong file or errors in json file
+     * checks for the adventure being null null adventure is caused by the wrong
+     * file or errors in json file
      */
     private void checkNullAdventure() {
         outputArea.setText("");
-        if(adventure == null) {
+        if (adventure == null) {
             loadDefaultAdventure();
             game.possibleNewPlayer(adventure, "No name", filename);
         } else {
@@ -106,11 +111,10 @@ public class GameView extends JFrame {
     }
 
     /**
-     * loads the default adventure
-     * used for when the loaded adventure is null
+     * loads the default adventure used for when the loaded adventure is null
      */
     private void loadDefaultAdventure() {
-        adventure = game.gameIntro(new String[] {""});
+        adventure = game.gameIntro(new String[] { "" });
         filename = "default_adventure.json";
         outputArea.append(game.getErrorMessage() + "\nFile could not be opened.\n");
         outputArea.append("Please use the menu to load a file, or use our default adventure.\n\n");
@@ -135,7 +139,7 @@ public class GameView extends JFrame {
         contentPane.add(playPanel(), BorderLayout.CENTER);
         contentPane.add(rightPanel(), BorderLayout.LINE_END);
         contentPane.add(bottomPanel(), BorderLayout.PAGE_END);
-        
+
     }
 
     /**
@@ -144,12 +148,12 @@ public class GameView extends JFrame {
     private JPanel bottomPanel() {
         JPanel bottomPanel = new JPanel();
         bottomPanel.setBorder(BorderFactory.createLineBorder(Color.RED));
-        return(bottomPanel);
+        return (bottomPanel);
     }
 
     /**
-     * @return the name jpanel
-     * this jpanel holds the adventure and player name JLabel
+     * @return the name jpanel this jpanel holds the adventure and player name
+     *         JLabel
      */
     private JPanel namePanel() {
         JPanel namePanel = new JPanel();
@@ -157,18 +161,17 @@ public class GameView extends JFrame {
         namePanel.add(adventureName);
         playerName.setBorder(new EmptyBorder(NONE, NONE, MEDIUM, NONE));
         namePanel.add(playerName);
-        return(namePanel);
+        return (namePanel);
     }
 
     /**
-     * @return the play jpanel
-     * holds the input and output field
+     * @return the play jpanel holds the input and output field
      */
     private JPanel playPanel() {
         JPanel playPanel = new JPanel();
         playPanel.add(inputField());
         playPanel.add(outputArea());
-        return(playPanel);
+        return (playPanel);
     }
 
     /**
@@ -177,8 +180,8 @@ public class GameView extends JFrame {
     private JTextField inputField() {
         inputField.addActionListener(enterPressed -> doCommand(inputField));
         inputField.setColumns(BIGCOL);
-        inputField.setMargin(new Insets(SMALL,SMALL,SMALL,SMALL));
-        return(inputField);
+        inputField.setMargin(new Insets(SMALL, SMALL, SMALL, SMALL));
+        return (inputField);
     }
 
     /**
@@ -187,43 +190,41 @@ public class GameView extends JFrame {
     private JScrollPane outputArea() {
         outputArea.setColumns(BIGCOL);
         outputArea.setRows(ROWS);
-        outputArea.setMargin(new Insets(SMALL,SMALL,SMALL,SMALL));
+        outputArea.setMargin(new Insets(SMALL, SMALL, SMALL, SMALL));
         outputArea.setEditable(false);
         outputArea.setLineWrap(true);
         outputArea.setWrapStyleWord(true);
         outputArea.add(new JScrollPane());
         scrollPane = new JScrollPane(outputArea);
-        return(scrollPane);
+        return (scrollPane);
     }
 
     /**
-     * @return the right panel
-     * holds the menu panel and the inventory
+     * @return the right panel holds the menu panel and the inventory
      */
     private JPanel rightPanel() {
         JPanel rightPanel = new JPanel();
-        GridLayout layout = new GridLayout(2,0);
-        //layout.setVgap(SMALL);
+        GridLayout layout = new GridLayout(2, 0);
+        // layout.setVgap(SMALL);
         rightPanel.setLayout(layout);
         rightPanel.add(menuPanel());
         rightPanel.add(inventory());
-        return(rightPanel);
+        return (rightPanel);
     }
 
     /**
-     * @return the menu panel
-     * holds the menu label and all 4 buttons
+     * @return the menu panel holds the menu label and all 4 buttons
      */
     private JPanel menuPanel() {
         JPanel menuPanel = new JPanel();
-        menuPanel.setLayout(new GridLayout(SMALL,0));
+        menuPanel.setLayout(new GridLayout(SMALL, 0));
         menuPanel.add(menuLabelPanel());
         menuPanel.add(changeNameButton());
         menuPanel.add(loadJsonButton());
         menuPanel.add(loadSavedGameButton());
         menuPanel.add(saveButton());
         menuPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        return(menuPanel);
+        return (menuPanel);
     }
 
     /**
@@ -231,7 +232,7 @@ public class GameView extends JFrame {
      */
     private JLabel menuLabelPanel() {
         JLabel menuLabel = new JLabel("MENU", SwingConstants.CENTER);
-        return(menuLabel);
+        return (menuLabel);
     }
 
     /**
@@ -240,7 +241,7 @@ public class GameView extends JFrame {
     private JButton changeNameButton() {
         JButton changeNameButton = new JButton("Change Name");
         changeNameButton.addActionListener(buttonPressed -> changeName());
-        return(changeNameButton);
+        return (changeNameButton);
     }
 
     /**
@@ -249,7 +250,7 @@ public class GameView extends JFrame {
     private JButton loadJsonButton() {
         JButton loadJsonButton = new JButton("Load JSON file");
         loadJsonButton.addActionListener(buttonPressed -> loadFile("-a"));
-        return(loadJsonButton);
+        return (loadJsonButton);
     }
 
     /**
@@ -258,7 +259,7 @@ public class GameView extends JFrame {
     private JButton loadSavedGameButton() {
         JButton loadSavedGameButton = new JButton("Load saved game");
         loadSavedGameButton.addActionListener(buttonPressed -> loadFile("-l"));
-        return(loadSavedGameButton);
+        return (loadSavedGameButton);
     }
 
     /**
@@ -267,7 +268,7 @@ public class GameView extends JFrame {
     private JButton saveButton() {
         JButton saveButton = new JButton("Save game");
         saveButton.addActionListener(buttonPressed -> save());
-        return(saveButton);
+        return (saveButton);
     }
 
     /**
@@ -281,30 +282,30 @@ public class GameView extends JFrame {
         inventory.setWrapStyleWord(true);
         inventory.setFont(new Font("Arial", Font.BOLD, FONTSIZE));
         JScrollPane inventoryScroll = new JScrollPane(inventory);
-        return(inventoryScroll);
+        return (inventoryScroll);
     }
 
-    
     /* These are action listener methods that call methods from game */
-    /*################################################################*/
+    /* ################################################################ */
 
     /**
      * changes the name of the player
      */
     private void changeName() {
         String name = JOptionPane.showInputDialog("Enter your player's new name");
-        if(name == null) {
+        if (name == null) {
             return;
         }
-        //set the Game player name to the new name
-        game.changePlayerName(name,adventure);
-        //set the player jlabel to the new name
+        // set the Game player name to the new name
+        game.changePlayerName(name, adventure);
+        // set the player jlabel to the new name
         playerName.setText("Player Name: " + name);
         outputArea.append("Player name changed to " + name + "\n\n");
     }
 
     /**
      * does the command that the user enters
+     * 
      * @param textField - the text field where the user entered the command
      */
     private void doCommand(JTextField textField) {
@@ -326,11 +327,21 @@ public class GameView extends JFrame {
 
     /**
      * loads the file the user wants
+     * 
      * @param flag - the flags for how to open the file
      */
     private void loadFile(String flag) {
         askToSave();
-        String file = JOptionPane.showInputDialog("Enter the file path and name.");
+        String file = "";
+        JFileChooser fileChooser = new JFileChooser();
+        // fileChooser.setFileSelectionMode(JFileChooser.);
+        int fileChooserVal = fileChooser.showOpenDialog(this);
+        if (fileChooserVal == JFileChooser.APPROVE_OPTION) {
+            file = fileChooser.getSelectedFile().getAbsolutePath();
+            System.out.println(file);
+        }
+
+        //String file = JOptionPane.showInputDialog("Enter the file path and name.");
         if(file == null || file.equals("")) {
             return;
         }
